e2c2c8f025124c56446bad1586e82f5c5a911dfc
perhatt <3405256669@qq.com>
2023-11-22 13:42:27 +0800

    MusicList and MusicInfo Page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template class="container">
   <div class="realive w-screen h-screen flex justify-center items-center">
     <div
-      class="absolute w-4/5 h-[calc(100%-50px)] bg-[#141414] rounded-3xl border-2 border-spacing-1 border-white/40 flex flex-col">
+      class="absolute w-4/5 h-[calc(100%-50px)] bg-[#141414] overflow-hidden rounded-3xl border-2 border-spacing-1 border-white/40 flex flex-col">
       <router-view></router-view>
     </div>
     <img class="w-full h-full object-cover"
--- src/components/Player/Index.vue
+++ src/components/Player/Index.vue
@@ -5,7 +5,7 @@
       <div class="w-full h-20 rounded-b-3xl flex items-center px-2">
         <!-- 音乐图片 -->
         <div class=" w-16 h-16 bg-white/20 rounded-full flex items-center justify-center cursor-pointer musicImg"
-          @click="OpenPlayer = !OpenPlayer">
+          @click="OpenPlayer = !OpenPlayer, OpenList = false">
           <img class="w-14 h-14 rounded-full"
             src="https://tupian.qqw21.com/article/UploadPic/2020-6/202062820443133016.jpg" alt="">
         </div>
@@ -30,6 +30,10 @@
             <i class="iconfont icon-arrow-left-1"></i>
           </div>
 
+
+          <audio src="../../assets/music.mp3" @input="changePosition" :src="songUrl" ref="audioPlayer"
+            @ended="onEnded"></audio>
+
           <div @click="PlayMusic"
             class="w-10 h-10 hover:bg-red-500/50 bg-white/10 rounded-full text-lg flex justify-center items-center cursor-pointer">
             <i class="iconfont icon-play" :class="!play ? 'icon-play' : 'icon-pause'"></i>
@@ -41,42 +45,102 @@
           </div>
 
           <div class=" absolute -bottom-1 flex items-center justify-center w-full px-4  h-1  gap-2 text-xs">
-            <p>00:00</p>
-            <input type="range" v-model="currentTime" class="w-full h-full">
+            <p>{{ currentTime }}</p>
+            <input type="range" v-model="currentTime" min="0" max="100" step="1" @click="" class="w-full h-full">
             <p>04:03</p>
           </div>
         </div>
 
-        <!-- 列表 -->
-        <div class="w-12 h-12 flex items-center justify-center text-xl cursor-pointer hover:bg-white/10 rounded-lg ">
+        <!-- 列表  icon start-->
+        <div @click="OpenList = !OpenList"
+          class="w-12 h-12 flex items-center justify-center text-xl cursor-pointer hover:bg-white/10 rounded-lg ">
           <i class="iconfont icon-bullet-list"></i>
         </div>
+        <!-- 列表  icon end -->
       </div>
     </div>
+    <!-- Player Start -->
     <div v-if="OpenPlayer" class="w-full h-auto flex-1 p-4">
       {{ currentTime }}
     </div>
+    <!-- Player End -->
   </div>
+
+  <!-- LIst start -->
+  <div @click="OpenList = false" :class="OpenList ? '' : 'delay-100 origin-bottom-right translate-x-[200%]'"
+    class="absolute right-0 bootom-0 w-full h-[calc(100%-80px)]  transition-all duration-300 ease-in-out">
+    <ul @click.stop="OpenMusicList" @click="OpenList = true"
+      class=" w-64 h-[calc(100%-20px)] float-right rounded-bl-3xl bg-gray-900 border-bt-2 border-b border-l  border-white/40 border-spacing-1 transition-all duration-200 ease-in-out ">
+      <li class="w-full h-12 px-4  flex items-center">
+        <p class="text-white text-sm font-bold">音乐列表 {{ `( ${musicList.length} )` }}</p>
+      </li>
+      <li v-for="item in musicList" :key="item.name"
+        class="w-full h-12 px-2 flex items-center hover:bg-white/10 cursor-pointer">
+        <img class="w-10 h-10 rounded-xl" src="https://tupian.qqw21.com/article/UploadPic/2020-6/202062820443133016.jpg"
+          alt="">
+        <div class="max-w-[160px] h-12 px-1 flex flex-col justify-center">
+          <p class="text-xs font-bold whitespace-nowrap truncate">{{ item.name }}</p>
+          <p class="text-xs whitespace-nowrap truncate text-white/40">singer</p>
+        </div>
+        <div class="flex-1 h-10">
+          <div class="w-10 h-10 float-right icon-btn scale-75">
+            <i class="iconfont icon-close-"></i>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <!-- LIst end -->
 </template>
 <script>
+import { ref } from 'vue';
 export default {
   data() {
     return {
       OpenPlayer: false,
+      songUrl: '../assets/music.mp3',
+      OpenList: false,
       play: false,
-      currentTime: ''
+      currentTime: '12',
+      musicList: [
+        {
+          name: 'MusicName',
+          singer: 'Singer',
+          img: 'https://tupian.qqw21.com/article/UploadPic/2020-6/202062820443133016.jpg',
+          url: '',
+        }, {
+          name: 'MusicName',
+          singer: 'Singer',
+          img: 'https://tupian.qqw21.com/article/UploadPic/2020-6/202062820443133016.jpg',
+          url: '',
+        }, {
+          name: 'MusicName',
+          singer: 'Singer',
+          img: 'https://tupian.qqw21.com/article/UploadPic/2020-6/202062820443133016.jpg',
+          url: '',
+        }, {
+          name: 'MusicName',
+          singer: 'Singer',
+          img: 'https://tupian.qqw21.com/article/UploadPic/2020-6/202062820443133016.jpg',
+          url: '',
+        },
+      ],
     };
   },
   methods: {
     PlayMusic() {
       this.play = !this.play
-      let element = document.querySelector('.musicImg');
-      if (!this.play) {
-        element.style.animationPlayState = 'paused';
-      } else {
-        element.style.animationPlayState = 'running';
-        element.style.animation = 'spin 6s linear infinite';
-      }
+      // if (this.play) {
+      //   this.$refs.audioElement.pause()
+      //   this.play = false
+      // } else {
+      //   this.$refs.audioElement.play()
+      //   this.play = true
+      // }
+    },
+    changePosition(event) {
+      // this.$refs.audioElement.currentTime = event.target.value / 100 * this.$refs.audioElement.duration
+      console.log(event.target.value);
     },
 
   },
@@ -98,9 +162,6 @@ export default {
   border-radius: 20px;
 }
 
-/* 定义range控件容器的样式 */
-
-
 /* 定义range控件拇指的样式 */
 [type="range"]::-webkit-slider-thumb {
   -webkit-appearance: none;
@@ -117,3 +178,4 @@ export default {
   cursor: pointer;
 }
 </style>
+
